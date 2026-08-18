376781e78ec744e4d991a2eaa1e402791a1b76bf
waffle-promote <promote@waffle>
2026-08-18 19:14:13 +0000

    Promote waffle @ v0.13.0

---
+++ docs/assets/swimlanesDiagram-ULZ7WXOC-05nFC2DS.js
@@ -0,0 +1,8 @@
+import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-_wZywoZs.js";import"./chunk-I66GZJ75-BHxOINjD.js";import"./chunk-NSK5VX7P-Df8EIb5I.js";import"./chunk-4I5QYGJK-B6eozkC6.js";import"./chunk-WRU74C26-C2PrDAKC.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-UBXNYLIW-_U_veaa_.js";import"./chunk-2GRJ4B5K-DpGC7Xap.js";import"./chunk-XXDRQBXY-BuE3VzE_.js";import"./chunk-KBJHAD2P-CfXXYvOh.js";import"./chunk-W5SLKNZC-CV-eA7Fa.js";import"./chunk-QR6OTTB3-DQeCBvd2.js";import"./chunk-7Z6QIM7H-CrOMx723.js";import"./chunk-J7OUQ5F2-nUlnv4HB.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-JQJVKLGR-COZNiC6W.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
+  .swimlane.cluster rect {
+    stroke: ${e.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,`getStyles`)});export{r as diagram};
